5bfcbd650dff2fc880cfea8f50342c2c40e178bc
hramon46 <hector.collazo6@upr.edu>
2017-03-28 23:11:11 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import javax.swing.JFrame;
 
 public class Main {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Minesweeper");
+		JFrame myFrame = new JFrame("MINESWEEPER");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocation(400, 150);
 		myFrame.setSize(325, 380);
